3cc4d29b6b7630518dc3f0faee0e97d3d16660c4
nelumboo <tk.kielkowski@gmail.com>
2020-12-19 11:22:32 +0100

    Final

--- Kolokwium/Klasa.cs
+++ Kolokwium/Klasa.cs
@@ -1,7 +1,50 @@
-﻿namespace Kolokwium
+﻿// Stwórz klasę Przesyłka. Powinna zawierać właściwości Nadawca, Odbiorca, DataNadania, DataDoreczenia i Status. Status to enum o wartościach Nadana, WTransporcie i Doreczona. Dodaj konstruktor, który przyjmie parametr i wypełni informacjeNadawca
+//     i Odbiorca a także sprawdzi czy parametr Odbiorca nie jest pusty. Data nadania to data stworzenia obiektu, Status to Nadana. Dodaj funkcję Przyjmij która ustawi status na WTransporcie. Dodaj metodę Dorecz która ustawi status na Doreczona i uzupełni właściwość
+//     DataDoreczenia aktualną datą. 
+
+using System;
+
+namespace Kolokwium
 {
+    public enum Status
+    {
+        Nadana,
+        WTransporcie,
+        Doreczona
+    }
+
     public class Klasa
     {
-        
+        public string Nadawca { get; set; }
+        public string Odbiorca { get; set; }
+        public DateTime DataNadania { get; set; }
+        public DateTime DataDoreczenia { get; set; }
+        public Status Status { get; set; }
+
+        public Klasa(string nadawca, string odbiorca)
+        {
+            Nadawca = nadawca;
+
+            if (string.IsNullOrWhiteSpace(odbiorca))
+                throw new AggregateException("Pole odbiorca nie moze byc puste");
+            else
+            {
+                Odbiorca = odbiorca;
+            }
+
+            DataNadania = DateTime.Now;
+            Status = Status.Nadana;
+        }
+
+        public void Dorecz()
+        {
+            Status = Status.Doreczona;
+            DataDoreczenia = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Nadawca)}: {Nadawca}, {nameof(Odbiorca)}: {Odbiorca}, {nameof(DataNadania)}: {DataNadania}, {nameof(DataDoreczenia)}: {DataDoreczenia}, {nameof(Status)}: {Status}";
+        }
     }
 }
--- Kolokwium/Program.cs
+++ Kolokwium/Program.cs
@@ -1,19 +1,46 @@
 ﻿using System;
+using System.Linq;
 
 namespace Kolokwium
 {
     class Program
     {
 
-        static void FUn()
+        static void Fun(int mm)
         {
-            
+           int profile = 2000 / mm;
+           double odpadki = 2000 - ((profile * mm) - profile);
+
+            Console.WriteLine($"Piłka wytnie {profile} profili i zostanie {odpadki} mm odpadków");
+
+        }
+
+        static void Fun2(string policz)
+        {
+            var zm = policz.Split(" ");
+            double size = zm.Length;
+            double ile = 0.24 * size;
+
+            Console.WriteLine($"W zadanym zdaniu znajduje się {size} słów. Do przeczytania potrzebne będzie {ile} sekund");
+
         }
         
         
         static void Main(string[] args)
         {
+            var a = 165;
+            Fun(a);
+
+            var zdanie = "Napisz funkcję, która przyjmie tekst a następnie obliczy ile jest w nim słów (np. na podstawie ilości spacji) a następnie obliczy ile czasu zajmie średnio przeczytanie tekstu. Średnia prędkość czytania to 250 słów na minutę. Funkcja powinna wypisać na ekran komunikat ile słów ma tekst i ile powinno zająć jego przeczytanie. Wywołaj funkcję na dowolnym tekście (np. 1 zdaniu).";
             
+            Fun2(zdanie);
+
+            Klasa paczka = new Klasa("Józek", "Kwiatkowski");
+
+            Console.WriteLine(paczka);
+            paczka.Dorecz();
+            Console.WriteLine(paczka);
+
         }
     }
 }
